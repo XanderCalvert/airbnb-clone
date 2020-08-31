e2e22c231fb66cab56ce6a2ffc507f591e0e88c6
Matt Calvert <xander.raymatt@gmail.com>
2020-09-01 00:14:46 +0100

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>© 2020 Airbnb, Inc. All rights reserved</p>
+      <p>Clone React App by Matt Calvert</p>
+    </div>
+  );
+}
+
+export default Footer;
